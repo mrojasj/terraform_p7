620432a4ecae41195362933bdc7783b3d86d0729
EC2 Default User <ec2-user@ip-10-0-0-149.ec2.internal>
2021-05-07 23:14:13 +0000

    Fix main

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ locals{
 }
 
 module "network" {
-  source = "git@github.com:mrojasj/tf_network_module.git"
+  source = "github.com/mrojasj/tf_network_module.git"
   vpc_cidr_block = var.vpc_cidr_block
   snet_extra_bits = var.snet_extra_bits
 }
 
 module "vm" {
-  source = "git@github.com:mrojasj/tf_vm_module.git"
+  source = "github.com/mrojasj/tf_vm_module.git"
   keyName = var.keyName
   vpc_id = module.network.vpc_id
   subnet_id = module.network.subnet_ids[0]
